68f9e41bac99ce01695c9a07808476c3cc1ede83
Franky Erra <frankyerra43@gmail.com>
2022-09-03 19:40:15 -0700

    assets folder

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Navtab.css'
+import './styles/Navtab.css'
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Pages.css'
 
 export default function Contact() {
   return (

2a4d97102ef793990ae695de5a804f6d7a1ab87a
Franky Erra <frankyerra43@gmail.com>
2022-09-11 14:17:14 -0700

    heroku push

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -8,24 +8,42 @@ export default function Portfolio() {
   return (
     <div className='portfolio-container mb-4'>
       <h2>Portfolio</h2>
+
       <div className='cards'>
-        <div className='project-containers'>
+        <div className='w-50'>
+        <div className='main-container'>
        <a id='main' href='https://group-2-recipe-saver.herokuapp.com/login'>
         <img src={recipe} className='main'/>
        </a>
        <p className='repos'><a href='https://github.com/echandlerdavis/Recipe-Saver'>Recipe Saver Repo</a></p>
        </div>
-       <div className='project-containers'>
+       </div>
+
+       <div className='d-flex flex-row flex-wrap w-50'>
+       <div className='other-containers'>
        <a href='https://reversedentistry.github.io/Random-Movie-Selector/'>
         <img src={movie} className='others'/>
        </a>
        <p className='repos'><a href='https://github.com/reversedentistry/Random-Movie-Selector'>Random Movies Repo</a></p>
        </div>
-       <div className='project-containers'>
+       <div className='other-containers'>
        <a href='https://fyerra1.github.io/weather_dashboard-SERVER_SIDE_API/'>
         <img src={weather} className='others'/>
        </a>
        <p className='repos'><a href='https://github.com/fyerra1/weather_dashboard-SERVER_SIDE_API'>Weather Forecast Repo</a></p>
+      </div>
+      <div className='other-containers'>
+       <a href='https://reversedentistry.github.io/Random-Movie-Selector/'>
+        <img src={movie} className='others'/>
+       </a>
+       <p className='repos'><a href='https://github.com/reversedentistry/Random-Movie-Selector'>Random Movies Repo</a></p>
+       </div>
+       <div className='other-containers'>
+       <a href='https://fyerra1.github.io/weather_dashboard-SERVER_SIDE_API/'>
+        <img src={weather} className='others'/>
+       </a>
+       <p className='repos'><a href='https://github.com/fyerra1/weather_dashboard-SERVER_SIDE_API'>Weather Forecast Repo</a></p>
+      </div>
       </div>
       </div>
     </div>
